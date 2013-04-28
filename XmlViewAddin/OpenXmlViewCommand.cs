32b6b0d2960519f9194022981651e99513a14eb7
BorisKozo <majorzbzzn@gmail.com>
2013-04-28 15:38:01 +0300

    Added a condition and the Reformat logic

--- XmlViewAddin/OpenXmlViewCommand.cs
+++ XmlViewAddin/OpenXmlViewCommand.cs
@@ -6,6 +6,7 @@ using System.Text;
 using System.Xml;
 using HP.Utt.UttDialog;
 using HP.LR.VuGen.XmlViewer;
+using System.Xml.Linq;
 
 namespace XmlViewAddin
 {
@@ -13,6 +14,11 @@ namespace XmlViewAddin
   {
     public static bool IsValidXml(string xml)
     {
+      if (string.IsNullOrWhiteSpace(xml))
+      {
+        return false;
+      }
+
       XmlDocument doc = new XmlDocument();
       try
       {
@@ -26,14 +32,14 @@ namespace XmlViewAddin
       return true;
     }
 
-    public override void Run()
+    public static string GetSelectedString()
     {
       ITextEditor editor = UttCodeEditor.GetActiveTextEditor();
       if (editor == null)
-        return;
-      string[] lines = editor.SelectedText.Split(new string[]{Environment.NewLine},StringSplitOptions.RemoveEmptyEntries);
+        return null;
+      string[] lines = editor.SelectedText.Split(new string[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries);
       StringBuilder result = new StringBuilder();
-      foreach (string line in lines) 
+      foreach (string line in lines)
       {
         string proccessedLine = line.Trim();
         //Remove the leading and trailing " - this is VuGen specific code
@@ -43,16 +49,22 @@ namespace XmlViewAddin
         }
         if (proccessedLine.EndsWith("\""))
         {
-          proccessedLine = proccessedLine.Remove(proccessedLine.Length-1, 1);
+          proccessedLine = proccessedLine.Remove(proccessedLine.Length - 1, 1);
         }
-        proccessedLine = proccessedLine.Replace("\\","");
-        
+        proccessedLine = proccessedLine.Replace("\\", "");
+
         result.Append(proccessedLine);
       }
-      bool isValid = IsValidXml(result.ToString());
-      if (isValid)
+
+      return result.ToString();
+    }
+
+    public override void Run()
+    {
+      string xml = GetSelectedString();
+      if (IsValidXml(xml))
       {
-        ShowXmlDialog(result.ToString());
+        ShowXmlDialog(xml);
       }
     }
 
@@ -66,7 +78,50 @@ namespace XmlViewAddin
       doc.LoadXml(xml);
       data.Document = doc;
       dialog.Content = content;
+      dialog.MaxWidth = 800;
+      dialog.AddOkButton();
+      dialog.AddButton("reformat", Reformat, System.Windows.Input.Key.R, System.Windows.Input.ModifierKeys.Alt, "Reformat");
       dialog.Show();
+    }
+
+    private void Reformat(CustomDialog dialog)
+    {
+      ITextEditor editor = UttCodeEditor.GetActiveTextEditor();
+      string selectedText = editor.SelectedText;
+      string[] xmlLines = FormatXml(GetSelectedString()).Split(new string[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries);
+      StringBuilder formattedText = new StringBuilder();
+      foreach (string line in xmlLines)
+      {
+        formattedText.AppendLine("\""+line.Replace("\"", "\\\"")+"\"");
+      }
+
+      formattedText.Remove(formattedText.Length - Environment.NewLine.Length, Environment.NewLine.Length);
+
+      if (!selectedText.StartsWith("\""))
+      {
+        formattedText.Insert(0, "\"" + Environment.NewLine);
+      }
+
+      if (!selectedText.EndsWith("\""))
+      {
+        formattedText.Append(Environment.NewLine+"\"");
+      }
+
+      editor.Document.Replace(editor.SelectionStart, editor.SelectionLength, formattedText.ToString());
+      dialog.Close();
+    }
+
+    private string FormatXml(String xml)
+    {
+      try
+      {
+        XDocument doc = XDocument.Parse(xml);
+        return doc.ToString();
+      }
+      catch (Exception)
+      {
+        return xml;
+      }
     }
   }
 }
